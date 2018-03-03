01319247271f0b8681df6422c92bf11c9fdf8535
Andrew Street <andrew_street@live.com>
2018-03-03 13:02:28 -0700

    comment crud

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,43 @@
 class CommentsController < ApplicationController
-  def index
-  end
+  before_action :set_comment, only: [:show, :edit, :update, :destroy]
+  # def index
+  #   @comments = Comments.all
+  # end
 
   def show
   end
 
   def new
+    @comment = Comment.new
   end
 
   def edit
   end
+
+  def create
+    @comment = Comment.new(comment_params)
+      if @comment.save
+        redirect_to movies_path
+      else
+        render :new
+      end
+  end
+
+  # def update
+  #   if @comment.update()
+  # end
+
+  def destroy
+    @comment.destroy
+    redirect_to @movie(:id)
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:body)
+    end
+
+    def set_comment
+      @comment = Comment.find(params[:id])
+    end
 end
